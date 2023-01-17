7d32b7c068167c56741b23c384a448e0197c8017
오민성 <qww1552@naver.com>
2023-01-17 12:13:06 +0900

    FEAT: 프로젝트 생성

---
+++ backend/arc205/src/test/java/com/project/arc205/Arc205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.arc205;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arc205ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
