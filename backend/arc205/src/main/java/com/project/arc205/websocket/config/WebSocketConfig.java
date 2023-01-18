a38ad5a23dcca6df203796bee87b86fc3457e5c8
구세영 <0917sy@gmail.com>
2023-01-18 17:22:44 +0900

    Feature/143 be init stomp

---
+++ backend/arc205/src/main/java/com/project/arc205/websocket/config/WebSocketConfig.java
@@ -0,0 +1,33 @@
+package com.project.arc205.websocket.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${allowed-origins}")
+    String[] allowedOrigins;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins(allowedOrigins)
+                .withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins(allowedOrigins);
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/pub");
+        config.enableSimpleBroker("/sub");
+    }
+
+}
