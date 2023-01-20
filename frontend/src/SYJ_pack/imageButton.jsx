e42d57a6f37fd2b985176d9591c7daa46a8164b2
moqoru <june6362@naver.com>
2023-01-20 14:14:48 +0900

    STYLE: 버튼 클래스 이름을 기능에 맞게 수정

--- frontend/src/SYJ_pack/imageButton.jsx
+++ frontend/src/SYJ_pack/imageButton.jsx
@@ -4,10 +4,10 @@ const imageButton = () => {
   return (
     <>
       {/* 버튼의 가로세로 비율은 8:5로 지정할 것 (원본 560x350px) */}
-      <button className="imgBtn floatingComponent" id="imgBtnFirst"><img className="imgBtnIcon" src="/btnIcons/iconSetting1.png" alt="설정"/></button>
-      <button className="imgBtn floatingComponent" id="imgBtnSecond"><img className="imgBtnIcon" src="/btnIcons/iconMission1.png" alt="미션"/></button>
-      <button className="imgBtn floatingComponent" id="imgBtnThird"><img className="imgBtnIcon" src="/btnIcons/iconAct1.png" alt="행동"/></button>
-      <button className="imgBtn floatingComponent" id="imgBtnForth"><img className="imgBtnIcon" src="/btnIcons/iconKill1.png" alt="살해"/></button>
+      <button className="imgBtn floatingComponent" id="settingBtn"><img className="imgBtnIcon" src="/btnIcons/iconSetting1.png" alt="설정"/></button>
+      <button className="imgBtn floatingComponent" id="missionBtn"><img className="imgBtnIcon" src="/btnIcons/iconMission1.png" alt="미션"/></button>
+      <button className="imgBtn floatingComponent" id="actBtn"><img className="imgBtnIcon" src="/btnIcons/iconAct1.png" alt="행동"/></button>
+      <button className="imgBtn floatingComponent" id="killBtn"><img className="imgBtnIcon" src="/btnIcons/iconKill1.png" alt="살해"/></button>
     </>
   );
 };
