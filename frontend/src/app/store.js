d3b1dc957285827fd1db225dcd0af893f8733187
parkdh9018 <parkdh9018@naver.com>
2023-01-20 10:37:03 +0900

    FEAT: Redux-saga 초기화

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga'
 import meReuducer from './me'
+import mySaga from './saga/sgags';
 
+const sagaMiddleware = createSagaMiddleware()
 export const store = configureStore({
   reducer: {
     me: meReuducer,
   },
-});
+  middleware: [sagaMiddleware]
+},
+);
+
+sagaMiddleware.run(mySaga);
