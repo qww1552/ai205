5a910072982340fae576eda7945d5b16d3f4f3a6
moqoru <june6362@naver.com>
2023-01-25 16:45:41 +0900

    FEAT: 화상회의 모달 창 추가

--- frontend/src/components/game/conference/modalConference.jsx
+++ frontend/src/components/game/conference/modalConference.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import { useState, useCallback } from 'react'
+import { Modal, Button } from 'antd'
+import 'styles/styles.css'
 
-const modalConference = () => {
+const ModalConference = () => {
+
+  const [open, setOpen] = useState(false);
+  const [userMedia, setUserMedia] = useState(false);
+
+  const handleShow = useCallback(() => setOpen(true), [setOpen]);
+
+  const handleClose = useCallback(
+    () => {
+      setOpen(false);
+      setUserMedia(false);
+    },
+    [setOpen, setUserMedia]
+  );
+
+  // const handleOnUserMedia = useCallback(() => setUserMedia(true), [
+  //   setUserMedia
+  // ]);
+
   return (
     <>
+      <Button type="primary" onClick={handleShow}>
+        Launch Modal Webcam
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        width={1000}
+        footer={[
+          <Button
+            key="Close"
+            type="primary"
+            onClick={handleClose}
+          >
+            Close
+          </Button>
+        ]}
+      >
+        <h1> 웹캠 화면 넣을 곳 </h1>
+        
+      </Modal>
       {/* {check && <div>
         투표 화면
       </div>}
@@ -14,4 +54,4 @@ const modalConference = () => {
 )
 };
 
-export default modalConference;
+export default ModalConference;
