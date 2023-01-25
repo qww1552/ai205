0585b89a024b0b8a3f305601f63c6d11bd5f1712
moqoru <june6362@naver.com>
2023-01-25 15:40:40 +0900

    FEAT: 회의 관련 파일을 담을 폴더 생성

---
+++ frontend/src/components/game/conference/modalConference.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const modalConference = () => {
+  return (
+    <>
+      {/* {check && <div>
+        투표 화면
+      </div>}
+      {!check && <div>
+        결과 화면
+      </div>} */}
+
+    </>
+)
+};
+
+export default modalConference;
