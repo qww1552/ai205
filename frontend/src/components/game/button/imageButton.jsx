8254b7e55b7a006a949c77aa9075d7d773217429
moqoru <june6362@naver.com>
2023-01-27 10:39:12 +0900

    FEAT: 행동 버튼이 회의 버튼에 다가갔을 때 활성화되도록 변경

--- frontend/src/components/game/button/imageButton.jsx
+++ frontend/src/components/game/button/imageButton.jsx
@@ -1,16 +1,24 @@
 import 'styles/styles.css'
+import { selectGameInfo } from "app/gameInfo"
+import { useSelector } from "react-redux"
+import { action } from "app/store"
 
-const imageButton = () => {
+const ImageButton = () => {
+
+  const isAdjacentConfer = useSelector(selectGameInfo).isAdjacentConfer
+
   return (
     <>
       {/* 버튼의 가로세로 비율은 8:5로 지정할 것 (원본 560x350px) */}
-      <button className="imgBtn floatingComponent" id="settingBtn"><img className="imgBtnIcon" src="/btnIcons/iconSetting1.png" alt="설정"/></button>
+      <button className="imgBtn floatingComponent"  id="settingBtn"><img className="imgBtnIcon" src="/btnIcons/iconSetting1.png" alt="설정"/></button>
       {/* 미션 버튼을 따로 만들 필요가 없어서 주석 처리 */}
       {/* <button className="imgBtn floatingComponent" id="missionBtn"><img className="imgBtnIcon" src="/btnIcons/iconMission1.png" alt="미션"/></button> */}
-      <button className="imgBtn floatingComponent" id="actBtn"><img className="imgBtnIcon" src="/btnIcons/iconAct1.png" alt="행동"/></button>
+
+      <button className={"imgBtnNoHover floatingComponent " + (isAdjacentConfer ? "imgBtnReady" : "")} id="actBtn" onClick={() => action('gameInfo/setAdjacentConferBtn', false) }><img className="imgBtnIcon" src="/btnIcons/iconAct1.png" alt="행동"/></button>
       <button className="imgBtn floatingComponent" id="killBtn"><img className="imgBtnIcon" src="/btnIcons/iconKill1.png" alt="살해"/></button>
     </>
   );
 };
 
-export default imageButton;
+
+export default ImageButton;
--- frontend/src/components/game/conference/modalConference.jsx
+++ frontend/src/components/game/conference/modalConference.jsx
@@ -1,27 +1,32 @@
-import { useState, useCallback } from 'react'
-import { Modal, Button } from 'antd'
-import 'styles/styles.css'
 import VideoComponent from 'components/webchat/VideoComponent'
+import { selectGameInfo } from "app/gameInfo"
+import { useSelector } from "react-redux"
+import { action } from "app/store"
+import { Row, Col, Button, Modal } from "antd"
 
 const ModalConference = () => {
 
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = useCallback(() => setOpen(true), [setOpen])
-  const handleClose = useCallback(() => setOpen(false), [setOpen])
+  const isInMeeting = useSelector(selectGameInfo).isInMeeting
 
   return (
     <>
       {/* 현재 게임 화면하단의 컴포넌트 + 회의때의 모달로 기능이 2가지로 분리되어 있는데도 불구,
       conference(회의)/modalConference 파일 하나에 2가지 기능이 몰려 있음
       -> redux를 이용해서 기능을 분화해야 함 */}
-       <VideoComponent />
-      {/* {check && <div>
-        투표 화면
-      </div>}
-      {!check && <div>
-        결과 화면
-      </div>} */}
+      {/* <VideoComponent /> */}
+      {/* <Modal
+        open={this.state.open}
+        onCancel={this.handleClose}
+        width={1000}
+        footer={[
+          <Button key="Close" type="primary" onClick={this.handleClose}>
+            Close
+          </Button>,
+        ]}
+      >
+        TEST
+      </Modal>
+      {!this.state.open ? (<div></div>) : null} */}
 
     </>
 )
