e3e11afe503c493a1a58c5de549210e5dd6bf1f0
parkdh9018 <parkdh9018@naver.com>
2023-01-27 16:43:18 +0900

    FIX: 캐릭터 이름 충돌판정 수정

--- frontend/src/components/game/mesh/characterMesh.jsx
+++ frontend/src/components/game/mesh/characterMesh.jsx
@@ -1,17 +1,21 @@
 import { Text } from "@react-three/drei";
+import { Suspense } from "react";
 import { Vector3 } from 'three';
 
-const CharacterMesh = ({initPosition, initColor, name}) => {
+const CharacterMesh = ({ initPosition, initColor, name }) => {
 
     return (
         <>
-                <Text fontSize={0.5} font='./SeoulHangangM.woff' position={new Vector3(0,0.2,0)} color="black" anchorX="center" anchorY="top-baseline">
+            <Suspense fallback={null}>
+                <Text fontSize={0.5} font='./SeoulHangangM.woff' position={new Vector3(0, 0.2, 0)} color="black" anchorX="center" anchorY="top-baseline">
                     {name}
                 </Text>
-                <mesh position={initPosition}>
-                    <boxGeometry args={[1, 1, 0.1]} />
-                    <meshStandardMaterial color={initColor} />
-                </mesh>
+            </Suspense>
+
+            <mesh position={initPosition}>
+                <boxGeometry args={[1, 1, 0.1]} />
+                <meshStandardMaterial color={initColor} />
+            </mesh>
         </>
     )
 }
