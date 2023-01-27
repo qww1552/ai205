6fa349dcd78e639f7e617de5c59c39732a9a10e7
moqoru <june6362@naver.com>
2023-01-27 11:30:45 +0900

    FIX: 긴급 회의 버튼에 다가가지 않아도 모달 창을 열 수 있는 문제를 수정

--- frontend/src/components/game/button/imageButton.jsx
+++ frontend/src/components/game/button/imageButton.jsx
@@ -5,17 +5,32 @@ import { action } from "app/store"
 
 const ImageButton = () => {
 
-  const isAdjacentConfer = useSelector(selectGameInfo).isAdjacentConfer
+  const isAdjacentMeetingBtn = useSelector(selectGameInfo).isAdjacentMeetingBtn
 
   return (
     <>
       {/* 버튼의 가로세로 비율은 8:5로 지정할 것 (원본 560x350px) */}
-      <button className="imgBtn floatingComponent"  id="settingBtn"><img className="imgBtnIcon" src="/btnIcons/iconSetting1.png" alt="설정"/></button>
+      <button
+        className="imgBtn floatingComponent"
+        id="settingBtn"
+      >
+          <img className="imgBtnIcon" src="/btnIcons/iconSetting1.png" alt="설정"/>
+      </button>
       {/* 미션 버튼을 따로 만들 필요가 없어서 주석 처리 */}
       {/* <button className="imgBtn floatingComponent" id="missionBtn"><img className="imgBtnIcon" src="/btnIcons/iconMission1.png" alt="미션"/></button> */}
-
-      <button className={"imgBtnNoHover floatingComponent " + (isAdjacentConfer ? "imgBtnReady" : "")} id="actBtn" onClick={() => action('gameInfo/setInMeeting', true) }><img className="imgBtnIcon" src="/btnIcons/iconAct1.png" alt="행동"/></button>
-      <button className="imgBtn floatingComponent" id="killBtn"><img className="imgBtnIcon" src="/btnIcons/iconKill1.png" alt="살해"/></button>
+      <button
+        className={"imgBtnNoHover floatingComponent " + (isAdjacentMeetingBtn ? "imgBtnReady" : "")}
+        id="actBtn"
+        onClick={isAdjacentMeetingBtn ? () => action('gameInfo/setInMeeting', true) : undefined}
+      >
+        <img className="imgBtnIcon" src="/btnIcons/iconAct1.png" alt="행동"/>
+      </button>
+      <button
+        className="imgBtn floatingComponent"
+        id="killBtn"
+      >
+        <img className="imgBtnIcon" src="/btnIcons/iconKill1.png" alt="살해"/>
+      </button>
     </>
   );
 };
