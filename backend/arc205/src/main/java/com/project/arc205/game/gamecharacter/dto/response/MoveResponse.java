d475b83a60a38741d1879385abcdc7f2175fcc72
오민성 <qww1552@naver.com>
2023-01-30 16:50:59 +0900

    FEAT: 출력 형식 수정
    BaseResponse 를 반환하도록 수정 (MoveResponse -> BaseResponse<MoveResponse>)

--- backend/arc205/src/main/java/com/project/arc205/game/gamecharacter/dto/response/MoveResponse.java
+++ backend/arc205/src/main/java/com/project/arc205/game/gamecharacter/dto/response/MoveResponse.java
@@ -3,7 +3,6 @@ package com.project.arc205.game.gamecharacter.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.project.arc205.game.gamecharacter.model.common.Location;
-import com.project.arc205.game.gamecharacter.operation.Action;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,8 +13,6 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 public class MoveResponse {
-
-    Action action;
     @JsonProperty("player")
     PlayerResponse playerResponse;
     Location location;
--- backend/arc205/src/main/java/com/project/arc205/game/gamecharacter/service/MoveService.java
+++ backend/arc205/src/main/java/com/project/arc205/game/gamecharacter/service/MoveService.java
@@ -3,14 +3,17 @@ package com.project.arc205.game.gamecharacter.service;
 import com.project.arc205.game.gamecharacter.dto.request.MoveRequest;
 import com.project.arc205.game.gamecharacter.dto.response.MoveResponse;
 import com.project.arc205.game.gamecharacter.dto.response.PlayerResponse;
-import com.project.arc205.game.gamecharacter.operation.Action;
+import com.project.arc205.game.gamecharacter.model.common.Location;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MoveService {
 
     public MoveResponse move(MoveRequest moveRequest) {
-        return new MoveResponse(Action.MOVE, new PlayerResponse(moveRequest.getPlayerRequest().getId()), moveRequest.getLocation());
+        String playerId = moveRequest.getPlayerRequest().getId();
+        PlayerResponse playerResponse = new PlayerResponse(playerId);
+        Location location = moveRequest.getLocation();
+        return new MoveResponse(playerResponse, location);
     }
 
 }
