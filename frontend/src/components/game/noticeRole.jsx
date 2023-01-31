c826c314a2ba515b7a7363422ecf6912ecdd336b
user name <user@inflearn.com>
2023-01-31 13:28:32 +0900

    feat: save2

---
+++ frontend/src/components/game/noticeRole.jsx
@@ -0,0 +1,15 @@
+
+import { selectMe } from 'app/me';
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const NoticeRole = () => {
+  const me = useSelector(selectMe).player
+  return (
+    <div>
+      <div>당신{me.player.name}의 역할:{me.player.role}</div>
+    </div>
+  );
+};
+
+export default NoticeRole;
