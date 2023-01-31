8b461fad612170ae54c16282599be4fbe3f7d278
user name <user@inflearn.com>
2023-01-31 13:28:14 +0900

    feat: save

--- frontend/src/components/game/meeting/webchatMeeting.jsx
+++ frontend/src/components/game/meeting/webchatMeeting.jsx
@@ -5,6 +5,7 @@ import { selectOhterPlayers } from '../../../app/others';
 import WebchatMeetingcomponent from './webchatMeetingcomponent';
 import { selectMe } from '../../../app/me';
 import { selectGameInfo } from '../../../app/gameInfo';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Todo: voteInfo에서 정보를 받아옴
 // import { selectVoteInfo } from '../../../app/voteInfo';
@@ -18,11 +19,11 @@ const WebchatMeeting = () => {
 
   const isInVoteResult = useSelector(selectGameInfo).isInVoteResult
   const [VoteduserInfo, setVoteduserinfo] = useState('')
-  const otherplayers = [{id:1, isAlive:true,isVote:true},{id:2, isAlive:true,isVote:true},{id:3, isAlive:true,isVote:true},{id:4, isAlive:true,isVote:true},{id:5, isAlive:true,isVote:true},{id:6, isAlive:false,isVote:false},{id:7, isAlive:false,isVote:false},{id:8, isAlive:false,isVote:false},{id:9, isAlive:false,isVote:false}]
-  const me = {id:'myid'}
+  const otherplayers = [{id:1, isAlive:true,isVote:true,},{id:2, isAlive:true,isVote:true},{id:3, isAlive:true,isVote:true},{id:4, isAlive:true,isVote:true},{id:5, isAlive:true,isVote:true},{id:6, isAlive:false,isVote:false},{id:7, isAlive:false,isVote:false},{id:8, isAlive:false,isVote:false}]
+  const me = {id:'myid', isAlive:false, isVote:true}
   // 누가 누구한테 투표했는지 투표결과를 저장할 변수, 나중에 주석해제
-  // const voteResult = useSelector(selectVoteInfo).vote_result
-  const voteResult = {1:['a','b','c'],2:['a','b','c'],3:['a','b','c'],4:['a','b','c'],5:['a','b','c'],6:['a','b','c'],7:['a','b','c'],8:['a','b','c'],9:['a','b','c'],'skip':['3535k']}
+  // const voteResult = useSelector(selectVoteInfo).voteResult
+  const voteResult = {1:['a','b','c'],2:['a','b','c'],3:['a','b','c'],4:['a','b','c'],5:['a','b','c'],6:['a','b','c'],7:['a','b','c'],8:['a','b','c'],'skip':['3535k']}
   const VoteEvent = (voteduserInfo) => {
     if (voteduserInfo.isAlive) {
       console.log(voteduserInfo.id)
@@ -37,17 +38,21 @@ const WebchatMeeting = () => {
   }
   return (
     <div>
-      
+      <WebchatMeetingcomponent className="col" userinfo={me}/>
+      <div className="row row-cols-3">
       {otherplayers.map((otherplayer) => (
         // Todo: 대충 props로 컴포넌트에 otherplayer정보를 넘겨준다
         <div key={otherplayer.id} onClick={()=>{VoteEvent(otherplayer)}}>
-        <WebchatMeetingcomponent userinfo={otherplayer} voteuser={voteResult[otherplayer.id]}/>
+        <WebchatMeetingcomponent className="col" userinfo={otherplayer} voteuser={voteResult[otherplayer.id]}/>
         </div>    
       ))}
       {/* Todo: 대충 props로 컴포넌트에 자기 정보를 넘겨준다 */}
-      <WebchatMeetingcomponent userinfo={me.id}/>
+      
+      </div>
       {/* isInVote가 실행되면 활성화 */}
-      <button onClick={submitEvent} disabled={!isInVote}>제출</button>
+      {/* 죽은 경우에는 투표를 못하니까 표시안함 */}
+      {me.isAlive === true&&<button onClick={submitEvent} disabled={!isInVote}>제출</button>}
+      
     </div>
     
   );
--- frontend/src/components/game/meeting/webchatMeetingcomponent.jsx
+++ frontend/src/components/game/meeting/webchatMeetingcomponent.jsx
@@ -9,7 +9,11 @@ const WebchatMeetingcomponent = (props) => {
   return (
     // props.userinfo.key 로 가져온다
     <div className='box'>
-      {props.userinfo.isAlive}
+      <div>{props.userinfo.id}</div>
+      {/* 자바스크립트에서 true false는 출력이 안된다 */}
+      {props.userinfo.isAlive === true? <div>살아있음</div> : <div>쥬금</div>}
+      <div>{props.userinfo.isVote}</div>
+      {/* 투표결과를 보여주는 경우 voteuser를 출력, 아닌경우 투표 여부를 출력 */}
       {isInVoteResult === true ? <div>{props.voteuser}</div>:props.userinfo.isVote === true? <div>투표완</div>:<div>투표안함</div>}
     </div>
   );
