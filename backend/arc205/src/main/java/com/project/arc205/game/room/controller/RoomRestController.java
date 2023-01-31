369a4e6bf8605a83e0be9e7f3ad74bef839f7a1e
오민성 <qww1552@naver.com>
2023-01-31 14:33:31 +0900

    FIX: 방 정보 조회 API 수정
    방 정보 조회를 REST API로 수정 (MessageMapping -> GetMapping)

--- backend/arc205/src/main/java/com/project/arc205/game/room/controller/RoomRestController.java
+++ backend/arc205/src/main/java/com/project/arc205/game/room/controller/RoomRestController.java
@@ -1,11 +1,14 @@
 package com.project.arc205.game.room.controller;
 
+import com.project.arc205.common.dto.BaseRestResponse;
 import com.project.arc205.game.room.dto.response.RoomListItemResponse;
+import com.project.arc205.game.room.dto.response.RoomResponse;
 import com.project.arc205.game.room.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +27,8 @@ public class RoomRestController {
         return ResponseEntity.ok(roomService.findAll());
     }
 
+    @GetMapping("/{room-id}")
+    public BaseRestResponse<RoomResponse> findById(@PathVariable("room-id") String roomId) {
+        return BaseRestResponse.of(roomService.findRoomById(roomId));
+    }
 }
