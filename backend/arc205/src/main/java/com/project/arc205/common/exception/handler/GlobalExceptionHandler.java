ea558f31e20dadaa8d114f4ac7e600a5c88a49df
Seyoung Goo <0917sy@gmail.com>
2023-01-31 10:48:41 +0900

    feat: handle MethodArgumentNotValidException

--- backend/arc205/src/main/java/com/project/arc205/common/exception/handler/GlobalExceptionHandler.java
+++ backend/arc205/src/main/java/com/project/arc205/common/exception/handler/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package com.project.arc205.common.exception.handler;
 import com.project.arc205.common.dto.ExceptionResponse;
 import com.project.arc205.common.exception.custom_exception.CustomException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
@@ -15,5 +17,11 @@ public class GlobalExceptionHandler {
     public ExceptionResponse handleException(CustomException e) {
 //        log.info("exception: {}", e.getMessage());
         return ExceptionResponse.of(e);
+    }
+
+    @MessageExceptionHandler(MethodArgumentNotValidException.class)
+    @SendToUser("/queue/errors")
+    public ExceptionResponse handleArgumentNotValidException(Exception e) {
+        return ExceptionResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 }
