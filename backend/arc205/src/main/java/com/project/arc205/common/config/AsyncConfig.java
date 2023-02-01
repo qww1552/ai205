e6b8aead3d056eee69cb21daaec7f7f95291d2fe
Seyoung Goo <0917sy@gmail.com>
2023-02-01 09:58:03 +0900

    feat: change voting end event. if finished before delay time, cancel the schedule

--- backend/arc205/src/main/java/com/project/arc205/common/config/AsyncConfig.java
+++ backend/arc205/src/main/java/com/project/arc205/common/config/AsyncConfig.java
@@ -1,17 +1,19 @@
 package com.project.arc205.common.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
-    @Bean
-    public Executor taskExecutor() {
+@EnableScheduling
+public class AsyncConfig implements AsyncConfigurer {
+    @Override
+    public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(2);    //기본 스레드 수
         executor.setMaxPoolSize(30);     //최대 스레드 수
--- backend/arc205/src/main/java/com/project/arc205/game/meeting/event/handler/MeetingEventHandler.java
+++ backend/arc205/src/main/java/com/project/arc205/game/meeting/event/handler/MeetingEventHandler.java
@@ -6,45 +6,51 @@ import com.project.arc205.common.operation.operation.MeetingOperation;
 import com.project.arc205.game.gamedata.model.entity.DummyGame;
 import com.project.arc205.game.meeting.event.VotingEndEvent;
 import com.project.arc205.game.meeting.event.MeetingEvent;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ScheduledFuture;
 
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class MeetingEventHandler {
     private final DummyGame curGame;  //TODO: change game repo
     private final SimpMessagingTemplate simpMessagingTemplate;
+    private final TaskScheduler taskScheduler;
     private final String destPrefix = "/sub/room/";
+
+    private Map<String, ScheduledFuture<?>> endSchedules;
 
     @Async
     @EventListener
-    public void meetingStart(MeetingEvent event) throws InterruptedException {
+    public void meetingStart(MeetingEvent event) {
         String destination = destPrefix + event.getRoomId();
         log.info("{}: start meeting event", destination);
         //TODO: Get from game setting
         long meetingLimitTime = 10 * 1000;
         long votingLimitTime = 20 * 1000;
 
-        //wait voting start
-        Thread.sleep(meetingLimitTime);
+        //schedule voting start
+        taskScheduler.schedule(() -> {
+            log.info("{}: StartVoting", destination);
+            BaseResponse<?> response = BaseResponse.of(Type.MEETING, MeetingOperation.START_VOTING);
+            simpMessagingTemplate.convertAndSend(destination, response);
+            curGame.getGameData().startVote();
 
-        log.info("{}: StartVoting", destination);
-        BaseResponse<?> response = BaseResponse.of(Type.MEETING, MeetingOperation.START_VOTING);
-        simpMessagingTemplate.convertAndSend(destination, response);
-        curGame.getGameData().startVote();
+            //schedule voting end
+            ScheduledFuture<?> endSchedule = taskScheduler.schedule(() -> votingEnd(new VotingEndEvent(event.getRoomId())), new Date(System.currentTimeMillis() + votingLimitTime));
+            endSchedules.put(event.getRoomId(), endSchedule);
 
-        //wait voting end
-        Thread.sleep(votingLimitTime);
-
-        if (curGame.getGameData().isVotingPeriod()) {
-            votingEnd(new VotingEndEvent(event.getRoomId()));
-        }
+        }, new Date(System.currentTimeMillis() + meetingLimitTime));
     }
 
     @Async
@@ -52,6 +58,13 @@ public class MeetingEventHandler {
     public void votingEnd(VotingEndEvent event) {
         String destination = destPrefix + event.getRoomId();
         log.info("{}: EndVoting", destination);
+
+        //cancel End schedule
+        if (endSchedules.containsKey(event.getRoomId())) {
+            endSchedules.remove(event.getRoomId()).cancel(true);
+        }
+
+        //broadcast voting result
         //TODO: 투표 결과 집계
         BaseResponse<?> response = BaseResponse.of(Type.MEETING, MeetingOperation.END);
         simpMessagingTemplate.convertAndSend(destination, response);
