65a35154ecabe7a75342ae1eb39dfd3962447c79
hwanguebest@naver.com <hwanguebest@naver.com>
2023-02-01 11:27:39 +0900

    FEAT: 함수형 컴포넌트로 변환

--- frontend/src/components/webchat/UserVideoComponent.js
+++ frontend/src/components/webchat/UserVideoComponent.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
-import OpenViduVideoComponent from './OvVideo';
-import './UserVideo.css';
+import React from "react";
+import OpenViduVideoComponent from "./OvVideo";
+import "./UserVideo.css";
 
-export default class UserVideoComponent extends Component {
-
-    getNicknameTag() {
-        // Gets the nickName of the user
-        return JSON.parse(this.props.streamManager.stream.connection.data).clientData;
-    }
-
-    render() {
-        return (
-            <div>
-                {this.props.streamManager !== undefined ? (
-                    <div className="streamcomponent">
-                        <OpenViduVideoComponent streamManager={this.props.streamManager} />
-                        <div><p>{this.getNicknameTag()}</p></div>
-                    </div>
-                ) : null}
-            </div>
-        );
-    }
+export default function UserVideoComponent(props) {
+  return (
+    <div>
+      {props.user.streamManager !== undefined ? (
+        <div className="streamcomponent">
+          <OpenViduVideoComponent
+            mutedSound={!props.user.audioActive}
+            mutedVideo={!props.user.videoActive}
+            streamManager={props.user.streamManager}
+          />
+          <div>
+            <p>{props.user.nickname}</p>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
 }
