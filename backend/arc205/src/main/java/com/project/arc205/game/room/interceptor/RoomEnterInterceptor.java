fb273b87ddae9e2d10f3ee03b17ff4397d201b48
오민성 <qww1552@naver.com>
2023-02-02 10:18:33 +0900

    REFACTOR: 코드 재정렬

--- backend/arc205/src/main/java/com/project/arc205/game/room/interceptor/RoomEnterInterceptor.java
+++ backend/arc205/src/main/java/com/project/arc205/game/room/interceptor/RoomEnterInterceptor.java
@@ -2,6 +2,7 @@ package com.project.arc205.game.room.interceptor;
 
 import com.project.arc205.game.gamecharacter.model.entity.Player;
 import com.project.arc205.game.room.service.RoomService;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
@@ -11,11 +12,8 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
-
 /**
- * Subscribe 요청을 가로채서 해당 Room에 입장시켜주는 인터셉터
- * url의 마지막 부분이 UUID 형태의 문자열인 room-id 이어야 함
+ * Subscribe 요청을 가로채서 해당 Room에 입장시켜주는 인터셉터 url의 마지막 부분이 UUID 형태의 문자열인 room-id 이어야 함
  */
 
 @Slf4j
@@ -24,6 +22,7 @@ import java.util.Objects;
 public class RoomEnterInterceptor implements ChannelInterceptor {
 
     private final RoomService roomService;
+
     private final String ROOM_SUBSCRIPTION_PREFIX = "/sub";
 
     @Override
@@ -55,11 +54,11 @@ public class RoomEnterInterceptor implements ChannelInterceptor {
         return accessor.getDestination().startsWith(ROOM_SUBSCRIPTION_PREFIX);
     }
 
-    private static boolean isSubscriptionCommand(StompHeaderAccessor accessor) {
+    private boolean isSubscriptionCommand(StompHeaderAccessor accessor) {
         return StompCommand.SUBSCRIBE.equals(accessor.getCommand());
     }
 
-    private static String getRoomIdFromHeader(StompHeaderAccessor accessor) {
+    private String getRoomIdFromHeader(StompHeaderAccessor accessor) {
         String destination = accessor.getDestination();
         return Objects.requireNonNull(destination).substring(destination.lastIndexOf("/") + 1);
     }
