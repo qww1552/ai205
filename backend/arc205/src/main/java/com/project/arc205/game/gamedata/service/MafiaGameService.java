03d161cd4c9024a2ca39148af1ad73aaaf017235
오민성 <qww1552@naver.com>
2023-02-03 10:54:57 +0900

    REFACTOR: GameCharacterAssignStrategy의 getGameCharacters 메소드 수정
    GameCharacterAssignStrategy가 Players를 매개변수로 전달받아 사용하도록 수정
    GameCharacter의 생성 책임을 Service로 옮김

--- backend/arc205/src/main/java/com/project/arc205/game/gamedata/service/MafiaGameService.java
+++ backend/arc205/src/main/java/com/project/arc205/game/gamedata/service/MafiaGameService.java
@@ -1,11 +1,14 @@
 package com.project.arc205.game.gamedata.service;
 
+import com.project.arc205.game.gamecharacter.model.entity.GameCharacter;
 import com.project.arc205.game.gamedata.dto.response.GameStartResponse;
 import com.project.arc205.game.gamedata.model.entity.GameData;
 import com.project.arc205.game.gamedata.model.entity.GameSetting;
 import com.project.arc205.game.gamedata.repository.GameRepository;
+import com.project.arc205.game.gamedata.strategy.GameCharacterAssignStrategy;
 import com.project.arc205.game.room.model.entity.Room;
 import com.project.arc205.game.room.repository.RoomRepository;
+import java.util.Map;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,11 +21,18 @@ public class MafiaGameService implements GameService {
 
     private final RoomRepository roomRepository;
     private final GameRepository gameRepository;
+    private final GameCharacterAssignStrategy gameCharacterAssignStrategy;
 
     @Override
     public GameStartResponse startGame(UUID roomId) {
         Room room = roomRepository.findById(roomId);
-        GameData gameData = gameRepository.create(room);
+
+        Map<String, GameCharacter> charactersFromPlayers = gameCharacterAssignStrategy.getCharactersFromPlayers(
+                room.getPlayers());
+
+        GameData gameData = gameRepository.create(roomId, room.getGameSetting(),
+                charactersFromPlayers);
+
         log.info("create new gameData :: {}", gameData);
         return GameStartResponse.of(roomId.toString());
     }
--- backend/arc205/src/main/java/com/project/arc205/game/gamedata/strategy/BasicGameCharacterAssignStrategy.java
+++ backend/arc205/src/main/java/com/project/arc205/game/gamedata/strategy/BasicGameCharacterAssignStrategy.java
@@ -1,6 +1,5 @@
 package com.project.arc205.game.gamedata.strategy;
 
-import com.project.arc205.common.model.Location;
 import com.project.arc205.game.gamecharacter.model.entity.Citizen;
 import com.project.arc205.game.gamecharacter.model.entity.GameCharacter;
 import com.project.arc205.game.gamecharacter.model.entity.Mafia;
@@ -9,34 +8,27 @@ import com.project.arc205.game.mission.BasicActiveMission;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import org.springframework.stereotype.Component;
 
 /**
  * players를 순회하며 마지막 player를 마피아로 선출 HashMap은 순서를 보장하지 않으므로 나름 랜덤
  */
+@Component
 public class BasicGameCharacterAssignStrategy implements
         GameCharacterAssignStrategy {
 
-    private final Map<String, Player> players;
-
-    private final Location initialLocation;
-
-    public BasicGameCharacterAssignStrategy(Map<String, Player> players, Location initialLocation) {
-        this.players = players;
-        this.initialLocation = initialLocation;
-    }
-
     @Override
-    public Map<String, GameCharacter> getCharacters() {
+    public Map<String, GameCharacter> getCharactersFromPlayers(Map<String, Player> players) {
         Map<String, GameCharacter> initialGameCharacters = new HashMap<>();
         String mafiaId = "";
 
         for (String id : players.keySet()) {
-            initialGameCharacters.put(id, new Citizen(initialLocation, Map.of(
+            initialGameCharacters.put(id, new Citizen(Map.of(
                     UUID.randomUUID().toString(), new BasicActiveMission())));
             mafiaId = id;
         }
 
-        initialGameCharacters.put(mafiaId, new Mafia(initialLocation, Map.of(
+        initialGameCharacters.put(mafiaId, new Mafia(Map.of(
                 UUID.randomUUID().toString(), new BasicActiveMission())));
 
         return initialGameCharacters;
