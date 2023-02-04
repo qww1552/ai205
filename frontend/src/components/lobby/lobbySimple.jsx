1ceeff5490db5a6b4e9870bdfc638dc4daca9dfa
parkdn9018 <parkdh9018@naver.com>
2023-02-04 12:07:34 +0900

    FIX : ohters 스토어에 내 정보가 들어가 있던 문제 수정

--- frontend/src/components/lobby/lobbySimple.jsx
+++ frontend/src/components/lobby/lobbySimple.jsx
@@ -1,38 +1,36 @@
 import { roomRequest } from 'api';
+import { selectMe } from 'app/me';
 import { action } from 'app/store';
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { Link, useRouteLoaderData } from 'react-router-dom';
 
 const LobbySimple = () => {
-  const [playerName, setPlayerName] = useState('');
 
-  // const players = [
-  //   { id : "player1" },
-  //   { id : "player2" }, 
-  // ]
-
-
-  const [players, setPlayers] = useState([])
-
+  const [otherPlayers, setotherPlayers] = useState([])
   const roomId = useRouteLoaderData("lobby");
+  const me = useSelector(selectMe);
 
   useEffect(()=> {
     action('SOCKET_CONNECT_REQUEST', {roomId})
   },[])
 
   useEffect(() => {
-    document.title = `Waiting Room - ${players.length} players`;
-  }, [players]);
+    document.title = `Waiting Room - ${otherPlayers.length} otherPlayers`;
+  }, [otherPlayers]);
 
   useEffect(() => {
     const timer = setInterval(() => {
       roomRequest(roomId).then(res =>  {
+        const players = res.data.data.players
+        const otherPlayers = players.filter(v => (v.id !== 'master')&&(v.id !== me.player.id))
+        setotherPlayers(otherPlayers);
 
-        const players = res.data.data.players.filter(v => v.id != 'master')
-        setPlayers(players);
-        for(const player of players) {
+        for(const player of otherPlayers) {
           action('others/setOtherPlayer', {player, location : {x : 0, y: 0}})
         }
+      }).catch((e) => {
+        setotherPlayers([]);
       });
     }, 2000)
     return () => clearInterval(timer);
@@ -41,13 +39,15 @@ const LobbySimple = () => {
   return (
     <div className="waiting-room">
       <h1>Waiting Room</h1>
-      <h2>Players</h2>
+      <h2>Me</h2>
+        {me.player.id}
+      <h2>OtherPlayers</h2>
       <ul>
-        {players.map(player => (
-          <li key={player.id}>{player.id}</li>
+        {otherPlayers.map((player, i) => (
+          <li key={`${player.id}${i}`}>{player.id}</li>
         ))}
       </ul>
-      {players.length >= 2 && (
+      {otherPlayers.length >= 1 && (
         <Link to={`/rooms/${roomId}/game`}><button>Start Game</button></Link>
       )}
     </div>
