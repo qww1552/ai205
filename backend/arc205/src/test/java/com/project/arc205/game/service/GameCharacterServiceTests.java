4a8cc62c73455357c176734b55e36766a3cc222b
오민성 <qww1552@naver.com>
2023-02-06 14:20:35 +0900

    TEST: 마피아 - 살해 기능 테스트

---
+++ backend/arc205/src/test/java/com/project/arc205/game/service/GameCharacterServiceTests.java
@@ -0,0 +1,75 @@
+package com.project.arc205.game.service;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.project.arc205.game.gamecharacter.exception.OnlyMafiaCanKillException;
+import com.project.arc205.game.gamecharacter.model.entity.Citizen;
+import com.project.arc205.game.gamecharacter.model.entity.GameCharacter;
+import com.project.arc205.game.gamecharacter.model.entity.Mafia;
+import com.project.arc205.game.gamecharacter.service.GameCharacterService;
+import com.project.arc205.game.gamedata.model.entity.GameData;
+import com.project.arc205.game.gamedata.model.entity.GameSetting;
+import com.project.arc205.game.gamedata.repository.GameRepository;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class GameCharacterServiceTests {
+
+    @Mock
+    GameRepository gameRepository;
+
+    GameCharacterService gameCharacterService;
+
+    @BeforeEach
+    void init() {
+        this.gameCharacterService = new GameCharacterService(gameRepository);
+    }
+
+    @Test
+    @DisplayName("마피아는 시민을 살해할 수 있다.")
+    void mafiaCanKillCitizen() {
+        Map<String, GameCharacter> gameCharacterMap = new HashMap<>();
+        Mafia mafia = new Mafia(null);
+        Citizen citizen = new Citizen(null);
+        gameCharacterMap.put("p1", mafia);
+        gameCharacterMap.put("p2", citizen);
+        when(gameRepository.findById(any()))
+                .thenReturn(GameData.of(new GameSetting(), gameCharacterMap));
+
+        gameCharacterService.kill(UUID.randomUUID(), "p1", "p2");
+
+        assertFalse(citizen.getIsAlive());
+        assertThat(mafia.getKillCount(), equalTo(1));
+    }
+
+    @Test
+    @DisplayName("마피아가 아닌 GameCharacter는 상대방을 살해할 수 없다.")
+    void onlyMafiaCanKill() {
+        Map<String, GameCharacter> gameCharacterMap = new HashMap<>();
+        Mafia mafia = new Mafia(null);
+        Citizen citizen = new Citizen(null);
+        gameCharacterMap.put("p1", mafia);
+        gameCharacterMap.put("p2", citizen);
+        when(gameRepository.findById(any()))
+                .thenReturn(GameData.of(new GameSetting(), gameCharacterMap));
+
+        assertThrows(OnlyMafiaCanKillException.class, () ->
+                gameCharacterService.kill(UUID.randomUUID(), "p2", "p1")
+        );
+    }
+
+
+}
