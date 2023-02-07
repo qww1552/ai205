5b41c426b2a4bcaa7cb683313fca4f0b9bb238e2
user name <user@inflearn.com>
2023-02-07 14:19:10 +0900

    FEAT: 투표결결과 컴포넌트 연결

--- frontend/src/components/game/meeting/resultMeeting.jsx
+++ frontend/src/components/game/meeting/resultMeeting.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-// import {selectResult} from '../../../app/result'
+import { selectVoteInfo } from '../../../app/voteInfo';
 
-// import { selectVoteInfo } from '../../../app/voteInfo';
 
 const ResultMeeting = () => {
 
-  // const result = useSelector(selectVoteInfo);
-  const result = {vote_result:[],elected : { id : "player3", role: "citizen"}}
+  const result = useSelector(selectVoteInfo).elected;
+
 
   return (
     <div>
       {/* 대충 당선된사람 띄움 */}
-      {result.elected.id}가 당선됨
-      {result.elected.role}역할이었음
+      {result.id}가 당선됨
+      {result.role}역할이었음
     </div>
   
   )
