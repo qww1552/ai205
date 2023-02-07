8341bba54f4ddbc6120b6e44033db982fb45ea3b
moqoru <june6362@naver.com>
2023-02-07 15:41:56 +0900

    Revert "TEST: 티쳐블 머신 테스트"
    This reverts commit c77b3c7ec54a7c82a5ebce93d53f38553ef1642e.

--- frontend/src/components/lobby/mainSimple.jsx
+++ frontend/src/components/lobby/mainSimple.jsx
@@ -1,97 +1,6 @@
-import { useRef, useEffect } from 'react';
 import { Link, Outlet } from "react-router-dom";
-import useScript from "./useScript"
-import * as tf from '@tensorflow/tfjs'
-import * as tmPose from '@teachablemachine/pose';
 
 const MainSimple = () => {
-
-    const [loading, error] = useScript("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js", "https://cdn.jsdelivr.net/npm/@teachablemachine/pose@0.8/dist/teachablemachine-pose.min.js");
-    if (error) console.log("에러 발생");
-    if (loading) console.log("로드 중...");
-
-    const URL = "https://teachablemachine.withgoogle.com/models/Y9Ln5KVkH/";
-    let model, webcam, ctx, labelContainer, maxPredictions;
-    // const requestRef = useRef(0)
-
-    async function init() {
-        const modelURL = URL + "model.json";
-        const metadataURL = URL + "metadata.json";
-
-        // load the model and metadata
-        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-        // Note: the pose library adds a tmPose object to your window (window.tmPose)
-        model = await tmPose.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
-
-        // Convenience function to setup a webcam
-        const size = 200;
-        const flip = true; // whether to flip the webcam
-        webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
-        await webcam.setup(); // request access to the webcam
-        await webcam.play();
-        // windowRender()
-        window.requestAnimationFrame(loop); // 첫 렌더링
-
-        // append/get elements to the DOM
-        const canvas = document.getElementById("canvas");
-        canvas.width = size; canvas.height = size;
-        ctx = canvas.getContext("2d");
-        labelContainer = document.getElementById("label-container");
-        for (let i = 0; i < maxPredictions; i++) { // and class labels
-            labelContainer.appendChild(document.createElement("div"));
-        }
-    }
-
-    // const windowRender = timestamp => {
-    //     requestRef.current = requestAnimationFrame(loop)
-    // }
-
-    async function loop(timestamp) {
-        webcam.update(); // update the webcam frame
-        await predict();
-        // windowRender()
-        window.requestAnimationFrame(loop);
-    }
-    
-    // useEffect(() => {
-    //     if (webcam) {
-    //         requestRef.current = requestAnimationFrame(loop)
-    //         return () => cancelAnimationFrame(requestRef.current)
-    //     }
-    // }, [])
-
-    async function predict() {
-        // Prediction #1: run input through posenet
-        // estimatePose can take in an image, video or canvas html element
-        // console.log(webcam.canvas)
-        // console.log(model)
-        const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
-        // Prediction 2: run input through teachable machine classification model
-        const prediction = await model.predict(posenetOutput);
-
-        for (let i = 0; i < maxPredictions; i++) {
-            const classPrediction =
-                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-            labelContainer.childNodes[i].innerHTML = classPrediction;
-        }
-
-        // finally draw the poses
-        drawPose(pose);
-    }
-
-    function drawPose(pose) {
-        if (webcam.canvas) {
-            ctx.drawImage(webcam.canvas, 0, 0);
-            // draw the keypoints and skeleton
-            if (pose) {
-                const minPartConfidence = 0.5;
-                tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
-                tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
-            }
-        }
-    }
-
     return (<>
         <div>
             <h1>MainPage</h1>
@@ -99,10 +8,6 @@ const MainSimple = () => {
             <Outlet>
                 
             </Outlet>
-            <div>Teachable Machine Pose Model</div>
-            <button type="button" onClick={init}>Start</button>
-            <div><canvas id="canvas"></canvas></div>
-            <div id="label-container"></div>
         </div>
     </>)
 }
