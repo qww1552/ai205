6e7f565c5eacdc94715fdcb782eee55348c13069
Seyoung Goo <0917sy@gmail.com>
2023-02-08 17:31:33 +0900

    refactor: 불필요한 상수 삭제

--- backend/arc205/src/main/java/com/project/arc205/common/util/Constant.java
+++ backend/arc205/src/main/java/com/project/arc205/common/util/Constant.java
@@ -7,6 +7,4 @@ public class Constant {
 
     public static final String VOTED_SKIP_ID = "skip";
     public static final String DESTINATION_PREFIX = "/sub/room/";
-
-    public static String TEMP_ROOM_ID = "0c827963-6661-5511-92f9-dd7d375b968d";
 }
