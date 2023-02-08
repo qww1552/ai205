5e9e2cde611a775f47ad7bf5d593335fd8a29700
Seyoung Goo <0917sy@gmail.com>
2023-02-08 16:56:34 +0900

    refactor: move Constant package

--- backend/arc205/src/main/java/com/project/arc205/common/util/Constant.java
+++ backend/arc205/src/main/java/com/project/arc205/common/util/Constant.java
@@ -1,4 +1,4 @@
-package com.project.arc205.common;
+package com.project.arc205.common.util;
 
 import org.springframework.stereotype.Component;
 
@@ -8,5 +8,5 @@ public class Constant {
     public static final String VOTED_SKIP_ID = "skip";
     public static final String DESTINATION_PREFIX = "/sub/room/";
 
-    public static final String TEMP_ROOM_ID = "0c827963-6661-5511-92f9-dd7d375b968d";
+    public static String TEMP_ROOM_ID = "0c827963-6661-5511-92f9-dd7d375b968d";
 }
