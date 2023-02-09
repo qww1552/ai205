08627c552359e5ebb5162d4d887806a9a8d227ea
Seyoung Goo <0917sy@gmail.com>
2023-02-09 17:27:20 +0900

    refactor: remove duplicated send die, you_died message

--- backend/arc205/src/main/java/com/project/arc205/game/gamecharacter/service/GameCharacterService.java
+++ backend/arc205/src/main/java/com/project/arc205/game/gamecharacter/service/GameCharacterService.java
@@ -1,7 +1,6 @@
 package com.project.arc205.game.gamecharacter.service;
 
 import com.project.arc205.common.model.Location;
-import com.project.arc205.game.gamecharacter.dto.response.KillBroadcastResponse;
 import com.project.arc205.game.gamecharacter.dto.response.MoveResponse;
 import com.project.arc205.game.gamecharacter.exception.OnlyMafiaCanKillException;
 import com.project.arc205.game.gamecharacter.model.entity.Citizen;
@@ -37,7 +36,7 @@ public class GameCharacterService {
     }
 
     @Transactional
-    public KillBroadcastResponse kill(UUID uuid, String mafiaPlayerId, String citizenPlayerId) {
+    public void kill(UUID uuid, String mafiaPlayerId, String citizenPlayerId) {
         GameData gameData = gameRepository.findById(uuid);
 
         GameCharacter gameCharacter = gameData.getGameCharacter(mafiaPlayerId);
@@ -49,11 +48,5 @@ public class GameCharacterService {
         Mafia mafia = (Mafia) gameCharacter;
         Citizen citizen = (Citizen) gameData.getGameCharacter(citizenPlayerId);
         mafia.kill(citizen);
-
-        KillBroadcastResponse.Player playerResponse = KillBroadcastResponse.Player.of(
-                citizenPlayerId,
-                citizen.getRole().name(), citizen.getIsAlive());
-
-        return new KillBroadcastResponse(playerResponse, citizen.getLocation());
     }
 }
