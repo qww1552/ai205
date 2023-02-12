a18e0c9fd07b1066b752681547c08b3e279b74ae
moqoru <june6362@naver.com>
2023-02-12 15:34:53 +0900

    FEAT: 3초 유지 미션 기능 구현(초기화 기능 미구현)

--- frontend/src/components/lobby/mainSimple.jsx
+++ frontend/src/components/lobby/mainSimple.jsx
@@ -1,7 +1,29 @@
 import Teachable from "components/teachable/Teachable";
 import { Link, Outlet } from "react-router-dom";
+import { Modal, Button } from 'antd'
+import { useState } from 'react'
 
 const MainSimple = () => {
+    const [testModalOpen, setTestModalOpen] = useState(false)
+    const [fileUrl, setFileUrl] = useState("")
+    const [poseInfo, setPoseInfo] = useState("")
+    const [missionName, setMissionName] = useState("")
+
+    // 모달창 끄고 켜는 것에 display none을 연동시키려 했지만 잘 안 됨
+
+    // const teachableCanvas = document.getElementById("divCanvas")
+
+    // const showCanvas = () => {
+    //     setTimeout(() => {
+    //         teachableCanvas.style.display = "block"
+    //     }, 2000)
+    // }
+    // const hideCanvas = () => {
+    //     setTimeout(() => {
+    //         teachableCanvas.style.display = "none"
+    //     }, 2000)
+    // }
+
     return (<>
         <div>
             <h1>MainPage</h1>
@@ -9,7 +31,49 @@ const MainSimple = () => {
             <Outlet>
                 
             </Outlet>
-            {/* <Teachable myurl="/heart_model/" /> */}
+            <br></br>
+            {/* Teachable.js의 useEffect 구문에 [props] 추가함 */}
+            <Button
+                type="primary"
+                onClick={() => {
+                    // showCanvas()
+                    setTestModalOpen(true)
+                    setFileUrl("/my_model/")
+                    setPoseInfo("Class 2")
+                    setMissionName("X자 포즈")
+                }}
+            >
+                X자 모델 열기
+            </Button>
+            <Button
+                type="primary"
+                onClick={() => {
+                    // showCanvas()
+                    setTestModalOpen(true)
+                    setFileUrl("/heart_model/")
+                    setPoseInfo("heartLeft")
+                    setMissionName("왼쪽 팔 하트")
+                }}
+            >
+                하트 모델 열기
+            </Button>
+            <Modal
+                open={testModalOpen}
+                footer={[
+                    <Button
+                        key="submit"
+                        onClick={() => {
+                            // hideCanvas()
+                            setTestModalOpen(false)
+                        }}
+                    >
+                        창 닫기
+                    </Button>
+            ]}>
+                <h1> 당신의 미션은 {missionName}입니다! </h1>
+                <h2> 먼저 아래 버튼을 눌러 카메라를 테스트하세요! </h2>    
+                <Teachable myurl={fileUrl} poseInfo={poseInfo}/>
+            </Modal>
         </div>
     </>)
 }
--- frontend/src/components/teachable/Teachable.js
+++ frontend/src/components/teachable/Teachable.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Button, Progress } from 'antd'
 
 const Teachable = (props) => {
@@ -26,14 +26,34 @@ const Teachable = (props) => {
   //   }
   // }, [props.streamManager]);
   
+  const [teachableProgress, setTeachableProgress] = useState("미션 수행하기!")
+  // let currentPose = ""
+  // 비교용 변수를 만들어서 원하는 동작이 맞는 지 체크
+  let missionPose = props.poseInfo
+  let currentPoseTimer = 0
+  let missionPoseTimer = 3 // 3초 유지
+
   useEffect(() => {
     const btn = document.getElementById("teachable");
+    // 이 버튼 onclick에 setTeachableProgress를 또 넣을 수 있을까?
     btn.setAttribute("onclick", `init("${props.myurl}")`);
-  }, []);
+  }, [props]);
 
   const getPoseResult = () => {
     setTimeout(() => {
-      console.log(document.getElementById("poseResult").innerHTML)
+      let teachableTimer = setInterval(() => {
+        let poseResult = document.getElementById("poseResult").innerHTML
+        if (missionPose !== poseResult) {
+          currentPoseTimer = 0
+          setTeachableProgress(missionPose + " 동작이 감지되지 않았어요...")
+        } else if (currentPoseTimer < missionPoseTimer) {
+          currentPoseTimer += 1
+          setTeachableProgress(missionPose + " 동작을 " + currentPoseTimer + "초 동안 유지 중이에요!")
+        } else {
+          setTeachableProgress(missionPose + " 동작 유지 미션 완료!")
+          return clearInterval(teachableTimer)
+        }
+      }, 1000)
     }, 1000)
   }
   // try {
@@ -49,15 +69,15 @@ const Teachable = (props) => {
         <video id="teachable" autoPlay={true} muted={props.mutedSound} ref={videoRef} />
       </div> */}
       <Button id="teachable">
-        카메라 테스트하기
+        동작 정보 불러오기
       </Button>
       <div id="poseResult" style={{display:"none"}}> </div>
-      <div>
+      <div id="divCanvas">
         <canvas id="canvas"></canvas>
       </div>
       <div id="label-container"></div>
       <Button onClick={getPoseResult}>
-        미션 수행!
+        {teachableProgress}
       </Button>
     </>
   );
