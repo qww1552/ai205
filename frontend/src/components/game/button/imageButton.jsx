a051a167e6edc4cc879172e2b31aac2d670f6646
moqoru <june6362@naver.com>
2023-02-13 16:14:11 +0900

    FEAT: 미니맵 기능 추가, 버튼 재배치

--- frontend/src/components/game/button/imageButton.jsx
+++ frontend/src/components/game/button/imageButton.jsx
@@ -7,7 +7,7 @@ import { useSelector } from "react-redux"
 import { action } from "app/store"
 import { selectMe } from 'app/me'
 import { useKeyboardControls } from "@react-three/drei";
-import { Progress,Badge } from 'antd'
+import { Progress, Badge, Button, Modal } from 'antd'
 import './style.css'
 import { selectGameset } from 'app/gameSet'
 
@@ -20,6 +20,7 @@ const ImageButton = () => {
   const adjustPlayer = useSelector(selectMe).adjustPlayer
   const adjustBody = useSelector(selectMe).adjustBody
   const unReadMessage = useSelector(selectGameInfo).unReadMessage
+  const [minimapOpen, setMinimapOpen] = useState(false)
 
   const chatButtonActivate = () => {
     action('gameInfo/setChatModalOpen', true)
@@ -47,6 +48,10 @@ const ImageButton = () => {
       action('me/setAdjustBody', null);
     }
   }
+  const mapButtonToggle = () => {
+    setMinimapOpen(!minimapOpen)
+  }
+
   useEffect(()=>{console.log(unReadMessage)},[unReadMessage])
   // 게임 첫 시작의 쿨타임은 15초, 이후 10초로 설정
   const [killTimer, setKillTimer] = useState(-50)
@@ -92,6 +97,7 @@ const ImageButton = () => {
       {/* {me.isAlive === true? */}
       {/* 버튼의 가로세로 비율은 8:5로 지정할 것 (원본 560x350px) */}
       
+      {/* 오른쪽 버튼 : 시민, 마피아 공용 */}
       <button
         className="imgBtn floatingComponent"
         id="chatBtn"
@@ -100,25 +106,27 @@ const ImageButton = () => {
           <img className="imgBtnIcon" src="/btnIcons/iconChat1.png" alt="채팅"/>
           </Badge>
       </button>
-      
       <ChatComponent className="z-index2000"/>
-      {me.role === "MAFIA"?
+
       <button
-        className={"imgBtnNoHover floatingComponent " + ((adjustPlayer && killTimer >= 100) ? "imgBtnReady" : "")}
-        id="killBtn"
-        onClick={
-          (adjustPlayer && killTimer >= 100) ? () => {
-          killButtonActivate()
-          resetKillTimer()
-        } : undefined}
-      >
-        <Progress strokeWidth={4} percent={killTimer} steps={10} showInfo={false} strokeColor="red"/>
-        <p/>
-        <img className="imgBtnIcon" src="/btnIcons/iconKill1.png" alt="살해"/>
-        <p/>
-        <Progress strokeWidth={4} percent={killTimer} steps={10} showInfo={false} strokeColor="red"/>
+        className="imgBtn floatingComponent"
+        id="mapBtn"
+        onClick={mapButtonToggle}
+        >
+          <img className="imgBtnIcon" src="/btnIcons/iconMap1.png" alt="지도"/>
+          <Modal
+            title="미니맵"
+            width="76.5vh"
+            open={minimapOpen}
+            onCancel={mapButtonToggle}
+            footer={[]}
+          >
+            <div>
+              <img className="minimap" src="/map/labeledMapImage.png" alt="미니맵"/>
+            </div>
+          </Modal>
       </button>
-      :<div></div>}
+
       <button
         className={"imgBtnNoHover floatingComponent " + ((adjustBody) ? "imgBtnReady" : "")}
         id="reportBtn"
@@ -129,7 +137,7 @@ const ImageButton = () => {
         >
           <img className="imgBtnIcon" src="/btnIcons/iconReport1.png" alt="신고"/>
       </button>
-      <MissionComponent/>
+      
       <button
         className={"imgBtnNoHover floatingComponent " + ((isAdjacentMeetingBtn || (me.role!=="MAFIA" && isAdjacentMissionBtn)) ? "imgBtnReady" : "")}
         id="actBtn"
@@ -137,11 +145,42 @@ const ImageButton = () => {
           (isAdjacentMeetingBtn || (me.role!=="MAFIA" && isAdjacentMissionBtn)) ?
           actButtonActivate
           : undefined}
-        >
+          >
         <img className="imgBtnIcon" src="/btnIcons/iconAct1.png" alt="행동"/>
       </button>
+      <MissionComponent/>
+
+      {/* 왼쪽 영역 : 마피아 전용 */}
+      {me.role === "MAFIA"?
+      <>
+        <button
+          className={"imgBtnNoHover floatingComponent " + ((adjustPlayer && killTimer >= 100) ? "imgBtnReady" : "")}
+          id="killBtn"
+          onClick={
+            (adjustPlayer && killTimer >= 100) ? () => {
+            killButtonActivate()
+            resetKillTimer()
+          } : undefined}
+        >
+          <Progress strokeWidth={4} percent={killTimer} steps={10} showInfo={false} strokeColor="red"/>
+          <p/>
+          <img className="imgBtnIcon" src="/btnIcons/iconKill1.png" alt="살해"/>
+          <p/>
+          <Progress strokeWidth={4} percent={killTimer} steps={10} showInfo={false} strokeColor="red"/>
+        </button>
+
+        <button
+        className="imgBtn floatingComponent"
+        id="sabotageBtn"
+        onClick={undefined}
+        >
+          <img className="imgBtnIcon" src="/btnIcons/iconSabotage1.png" alt="방해"/>
+        </button>
+      </>
+      :undefined}
+
     </>
-    :<></>}
+    :undefined}
     </>
   );
 };
