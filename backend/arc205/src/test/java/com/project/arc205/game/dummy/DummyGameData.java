1578dee0fe52b9dac5861f33a12895d17116076e
오민성 <qww1552@naver.com>
2023-02-13 16:30:59 +0900

    FEAT: 테스트 코드 변경
    테스트용 객체 생성 코드 추가

--- backend/arc205/src/test/java/com/project/arc205/game/dummy/DummyGameData.java
+++ backend/arc205/src/test/java/com/project/arc205/game/dummy/DummyGameData.java
@@ -3,6 +3,8 @@ package com.project.arc205.game.dummy;
 import com.project.arc205.game.gamecharacter.model.entity.GameCharacter;
 import com.project.arc205.game.gamedata.model.entity.GameData;
 import com.project.arc205.game.gamedata.model.entity.GameSetting;
+import com.project.arc205.game.gamedata.strategy.BasicGameCharacterAssignStrategy;
+import com.project.arc205.game.room.model.entity.Room;
 import java.util.Map;
 import java.util.UUID;
 
@@ -13,4 +15,9 @@ public class DummyGameData {
         return GameData.of(UUID.randomUUID(), new GameSetting(), gameCharacterMap);
     }
 
+    public static GameData getEmptyTestGameDataFromRoom(Room testRoom) {
+        return GameData.of(testRoom.getId(), new GameSetting(),
+                new BasicGameCharacterAssignStrategy().getCharactersFromPlayers(
+                        testRoom.getPlayers()));
+    }
 }
