41ae15656240d3706b7ed099f2deb9d74965ce2e
parkdn9018 <parkdh9018@naver.com>
2023-02-13 22:28:49 +0900

    FIX: collider 최적화

--- frontend/src/components/game/canvas/map/gameMap.jsx
+++ frontend/src/components/game/canvas/map/gameMap.jsx
@@ -1,8 +1,12 @@
 import { useTexture } from "@react-three/drei";
 import { Vector3, RepeatWrapping } from "three";
-import { Suspense, useEffect, useMemo, useState } from "react";
-import data from "./mapData";
-import { BallCollider, CuboidCollider, RigidBody, useRapier } from "@react-three/rapier";
+import { memo, Suspense, useEffect, useMemo, useState } from "react";
+import obstacle from "./mapData";
+import {
+  BallCollider,
+  CuboidCollider,
+  RigidBody,
+} from "@react-three/rapier";
 import { missionsLocation, walls } from "config/mapObject";
 
 //80 x 60
@@ -10,86 +14,26 @@ import { missionsLocation, walls } from "config/mapObject";
 const width = 80;
 const height = 60;
 
-const GameMap = () => {
-  // const [obstacles, setObstacles] = useState([]);
-
+const GameMap = (() => {
 
   const obstacles = useMemo(() => {
     let result = [];
 
-        // wall data
-        data.layers[2].data.forEach((v, i) => {
-          if (v > 0) {
-
-            const row = (height - parseInt(i / width)) - (parseInt(height / 2) + 0.5)
-            const col = (i % width) - (parseInt(width / 2) - 0.5)
-
-            result = [
-              ...result,
-              {
-                id: "wall",
-                row,
-                col,
-              },
-            ]
-          }
-        });
-
-        // obstacle data
-        // data.layers[4].data.forEach((v, i) => {
-        //   if (v > 0) {
-        //     result = [
-        //       ...result,
-        //       {
-        //         id: "obs",
-        //         row: (height - parseInt(i / width)) - (parseInt(height / 2) + 0.5),
-        //         col: (i % width) - (parseInt(width / 2) - 0.5),
-        //       },
-        //     ]
-        //   }
-        // });
-
+    // obstacle data
+    obstacle.data.forEach((v, i) => {
+      if (v > 0) {
+        result = [
+          ...result,
+          {
+            id: "obs",
+            row: height - parseInt(i / width) - (parseInt(height / 2) + 0.5),
+            col: (i % width) - (parseInt(width / 2) - 0.5),
+          },
+        ];
+      }
+    });
     return result;
-
-  },[data])
-
-  // useEffect(() => {
-  //   console.log("fff")
-
-  //   // wall data
-  //   data.layers[2].data.forEach((v, i) => {
-  //     if (v > 0) {
-  //       setObstacles((prev) => [
-  //         ...prev,
-  //         {
-  //           id: "wall",
-  //           row: (height - parseInt(i / width)) - (parseInt(height / 2) + 0.5),
-  //           col: (i % width) - (parseInt(width / 2) - 0.5),
-  //         },
-  //       ]);
-  //     }
-
-
-  //   });
-
-  //   //obstacle data
-  //   data.layers[4].data.forEach((v, i) => {
-  //     if (v > 0) {
-  //       setObstacles((prev) => [
-  //         ...prev,
-  //         {
-  //           id: "obs",
-  //           row: (height - parseInt(i / width)) - (parseInt(height / 2) + 0.5),
-  //           col: (i % width) - (parseInt(width / 2) - 0.5),
-  //         },
-  //       ]);
-  //     }
-  //   });
-
-  //   // mission and meeting data index 5
-
-
-  // }, [data]);
+  }, [obstacle]);
 
   const originTexture = useTexture(`/map/mapImage.png`, (texture) => {
     texture.wrapS = texture.wrapT = RepeatWrapping;
@@ -113,13 +57,21 @@ const GameMap = () => {
               position={[v.col, v.row, 0]}
             />
           ))}
+          {obstacles.map((v) => (
+            <CuboidCollider
+              key={`${v.row}_${v.col}`}
+              args={[0.3, 0.3, 0.1]}
+              position={[v.col, v.row, 0]}
+            />
+          ))}
         </RigidBody>
       </Suspense>
 
       {/* 회의 버튼 */}
       <Suspense>
         <RigidBody type="fixed" sensor>
-          <BallCollider name="meeting"
+          <BallCollider
+            name="meeting"
             args={[0.5, 0.5, 0.1]}
             position={[5.5, -3.5, 0]}
           />
@@ -130,19 +82,19 @@ const GameMap = () => {
       <Suspense>
         <RigidBody type="fixed" sensor>
           {missionsLocation.map((v) => (
-            <BallCollider name={`mission_${v.id}`}
-              key={`${v.row}_${v.col}_${v.id}${Math.floor(Math.random() * 100)}`}
+            <BallCollider
+              name={`mission_${v.id}`}
+              key={`${v.row}_${v.col}_${v.id}${Math.floor(
+                Math.random() * 100
+              )}`}
               args={[0.5, 0.5, 0.1]}
               position={[v.col, v.row, 0]}
             />
           ))}
         </RigidBody>
       </Suspense>
-
-
-
     </>
   );
-};
+});
 
 export default GameMap;
--- frontend/src/config/mapObject.js
+++ frontend/src/config/mapObject.js
@@ -8,24 +8,50 @@ const missionsLocation = [
     { id : 7, col : 16.5, row : -13.5},
 ]
 
+// 공통 미션 좌표 16.5 17.5 (맨 오른쪽 위)
 
 const walls = [
     { row : 19, col : -15, width : 8, height : 1 },
     { row : 16, col : -24.5, width : 2, height : 5 },
-    { row : 11, col : -14, width : 9, height : 1 },
+    { row : 9, col : -14, width : 9, height : 2.9 },
     { row : 12, col : -5, width : 3.1, height : 1 },
-    { row : 8, col : -2.1, width : 1, height : 4 },
-    
-
+    { row : 8.5, col : -2.1, width : 1, height : 4.5 },
+    { row : 5.5, col : -4.1, width : 3, height : 1.5 },
+    { row : 2.5, col : -8.5, width : 2.5, height : 1.5 },
+    { row : 6, col : -10, width : 1, height : 3 },
+    { row : 0, col : -23.5, width : 0.5, height : 18 },
+    { row : -6.4, col : -16.5, width : 8, height : 2.3 },
+    { row : -1.7, col : -16.3, width : 0.3, height : 2.5 },
+    { row : -0, col : -18, width : 1.8, height : 0.8 },
+    { row : -2.5, col : -10.3, width : 4.7, height : 1.5 },
+    { row : -7, col : -7.3, width : 1.5, height : 6 },
+    { row : -10.5, col : -5.5, width : 1.5, height : 5.5 },
+    { row : -10, col : -2.6, width : 1.5, height : 2.8 },
+    { row : -11.1, col : -18, width : 0.3, height : 2.8 },
+    { row : -12.5, col : -15, width : 3, height : 1.5 },
+    { row : -19.5, col : -14, width : 10, height : 1.5 },
+    { row : -23.5, col : 1, width : 5, height : 2.5 },
+    { row : -20, col : 7.6, width : 1.7, height : 3 },
+    { row : -20, col : 15, width : 6, height : 1 },
+    { row : -15, col : 20, width : 1, height : 6 },
+    { row : -12.5, col : 18, width : 3, height : 1.5 },
+    { row : -13, col : 9, width : 4, height : 2 },
+    { row : -10, col : 20, width : 3, height : 1.5 },
+    { row : -10, col : 6, width : 3, height : 3 },
+    { row : -10, col : 9, width : 3, height : 1.5 },
+    { row : -6.5, col : 8, width : 1.5, height : 1.5 },
+    { row : -5, col : 9.2, width : 0.7, height : 4 },
+    { row : 5.1, col : 9.2, width : 0.7, height : 4 },
+    { row : 8.5, col : 6, width : 4, height : 4.5 },
+    { row : 7, col : 13, width : 6, height : 3 },
+    { row : 7, col : 24, width : 1, height : 3 },
+    { row : 9, col : 23, width : 1, height : 1 },
+    { row : 5, col : 24.5, width : 1, height : 15 },
+    { row : -0.5, col : 17, width : 1, height : 1.5 },
+    { row : 17, col : 0.7, width : 9.3, height : 2 },
+    { row : 19, col : 15, width : 9.3, height : 2 },
 ]
 
-// row, col, width, height, 1
-// -2, 3, 7, 1
 
-
-
-
-
-// 공통 미션 좌표 16.5 17.5 (맨 오른쪽 위)
 
 export { missionsLocation, walls };
