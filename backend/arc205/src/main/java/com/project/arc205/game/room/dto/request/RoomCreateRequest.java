fb9dbd17d578dff583053875824fbe6396c22d0d
user name <user@inflearn.com>
2023-02-13 14:30:45 +0900

    Merge branch 'origin/develop'

---
+++ backend/arc205/src/main/java/com/project/arc205/game/room/dto/request/RoomCreateRequest.java
@@ -0,0 +1,14 @@
+package com.project.arc205.game.room.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class RoomCreateRequest {
+
+    private String title;
+
+}
