8604f6b4ddf09b58b402aa93fd566c83b4fe0cad
Seyoung Goo <0917sy@gmail.com>
2023-02-13 17:21:13 +0900

    refactor: 캐릭 생성 전략 분리

--- backend/arc205/src/main/java/com/project/arc205/game/gamedata/manager/GameManager.java
+++ backend/arc205/src/main/java/com/project/arc205/game/gamedata/manager/GameManager.java
@@ -1,13 +1,21 @@
 package com.project.arc205.game.gamedata.manager;
 
+import com.project.arc205.common.model.Role;
+import com.project.arc205.game.gamecharacter.model.entity.Citizen;
 import com.project.arc205.game.gamecharacter.model.entity.GameCharacter;
+import com.project.arc205.game.gamecharacter.model.entity.Mafia;
+import com.project.arc205.game.gamecharacter.model.entity.Player;
 import com.project.arc205.game.gamedata.model.entity.GameData;
-import com.project.arc205.game.gamedata.strategy.GameCharacterAssignStrategy;
+import com.project.arc205.game.gamedata.strategy.ColorAssignStrategy;
+import com.project.arc205.game.gamedata.strategy.MissionDistributionStrategy;
+import com.project.arc205.game.gamedata.strategy.RoleAssignStrategy;
 import com.project.arc205.game.gamemap.model.entity.GameMap;
 import com.project.arc205.game.gamemap.model.repository.GameMapRepository;
+import com.project.arc205.game.mission.model.ActiveMission;
 import com.project.arc205.game.mission.model.entity.GameMapMission;
 import com.project.arc205.game.mission.model.repository.GameMapMissionRepository;
 import com.project.arc205.game.room.model.entity.Room;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +27,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class GameManager {
 
-    private final GameCharacterAssignStrategy gameCharacterAssignStrategy;
+    private final RoleAssignStrategy roleAssignStrategy;
+    private final MissionDistributionStrategy missionDistributionStrategy;
+    private final ColorAssignStrategy colorAssignStrategy;
     private final GameMapMissionRepository gameMapMissionRepository;
     private final GameMapRepository gameMapRepository;
 
@@ -29,12 +39,32 @@ public class GameManager {
         List<GameMapMission> missions = gameMapMissionRepository.findByGamemap(gameMap);
         int numberOfMissions = room.getGameSetting().getNumberOfMissions();
 
-        Map<String, GameCharacter> charactersFromPlayers = gameCharacterAssignStrategy.getCharactersFromPlayers(
-                room.getPlayers(), missions, numberOfMissions);
+        roleAssignStrategy.init(room.getPlayers().size());
+        missionDistributionStrategy.init(missions, numberOfMissions);
+        colorAssignStrategy.init();
 
-        GameData gameData = GameData.of(room.getId(), room.getGameSetting(), charactersFromPlayers,
+        Map<String, GameCharacter> charactersFromPlayers = createGameCharacter(room.getPlayers());
+
+        return GameData.of(room.getId(), room.getGameSetting(), charactersFromPlayers,
                 gameMap.getStartLocation());
+    }
 
-        return gameData;
+    private Map<String, GameCharacter> createGameCharacter(Map<String, Player> players) {
+        Map<String, GameCharacter> gameCharacters = new HashMap<>();
+
+        for (Player player : players.values()) {
+            String playerId = player.getId();
+            GameCharacter gameCharacter;
+            Role role = roleAssignStrategy.next();
+            Map<String, ActiveMission> missions = missionDistributionStrategy.next();
+            int color = colorAssignStrategy.next();
+            if (role == Role.CITIZEN) {
+                gameCharacter = new Citizen(playerId, missions, color);
+            } else {
+                gameCharacter = new Mafia(playerId, missions, color);
+            }
+            gameCharacters.put(playerId, gameCharacter);
+        }
+        return gameCharacters;
     }
 }
