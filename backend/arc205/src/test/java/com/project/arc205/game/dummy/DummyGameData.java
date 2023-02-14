dacc8dcf39d9335cce428df54211919678d8602e
Seyoung Goo <0917sy@gmail.com>
2023-02-14 12:47:02 +0900

    test: GameManager 변경으로 GameData 생성코드 수정

--- backend/arc205/src/test/java/com/project/arc205/game/dummy/DummyGameData.java
+++ backend/arc205/src/test/java/com/project/arc205/game/dummy/DummyGameData.java
@@ -1,11 +1,13 @@
 package com.project.arc205.game.dummy;
 
 import com.project.arc205.common.model.Location;
+import com.project.arc205.game.gamecharacter.model.entity.Citizen;
 import com.project.arc205.game.gamecharacter.model.entity.GameCharacter;
+import com.project.arc205.game.gamecharacter.model.entity.Mafia;
 import com.project.arc205.game.gamedata.model.entity.GameData;
 import com.project.arc205.game.gamedata.model.entity.GameSetting;
-import com.project.arc205.game.gamedata.strategy.BasicGameCharacterAssignStrategy;
 import com.project.arc205.game.room.model.entity.Room;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -18,8 +20,12 @@ public class DummyGameData {
     }
 
     public static GameData getEmptyTestGameDataFromRoom(Room testRoom) {
-        return GameData.of(testRoom.getId(), new GameSetting(),
-                new BasicGameCharacterAssignStrategy().getCharactersFromPlayers(
-                        testRoom.getPlayers()));
+        Map<String, GameCharacter> gameCharacters = new HashMap<>();
+        String mafiaId = UUID.randomUUID().toString();
+        gameCharacters.put(mafiaId, new Mafia(mafiaId, null, 1));
+        String citizenId = UUID.randomUUID().toString();
+        gameCharacters.put(citizenId, new Citizen(citizenId, null, 1));
+        return GameData.of(testRoom.getId(), new GameSetting(), gameCharacters,
+                new Location(0.0, 0.0));
     }
 }
