86bd732f86342a9142db3696595e5d5f2f63eae6
moqoru <june6362@naver.com>
2023-02-14 22:01:10 +0900

    FEAT: 이미지 가이드, 동작 설명 기능 완성

--- frontend/src/components/game/mission/teachableMission.jsx
+++ frontend/src/components/game/mission/teachableMission.jsx
@@ -2,46 +2,30 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Button, Progress } from 'antd'
 
 const TeachableMission = (props) => {
-  // const videoRef = useRef();
-  // useEffect(() => {
-  //   if (props && !!videoRef) {
-     
-
-  //     const canvas = document.querySelector('#canvas');
-  //     const ctx = canvas.getContext('2d');
-  //     let canPlayState = false;
-
-  //     ctx.textAlign = 'center';
-  //     ctx.fillText('비디오 로딩 중..', 300, 200);
-
-  //     const videoElem = document.querySelector('#teachable');
-  //     videoElem.addEventListener('canplaythrough', render);
-
-  //     function render() {
-  //       ctx.drawImage(videoElem, 0, 0, 600, 400);
-  //       // 첫 번째 인자로 비디오를 넣어준다.
-  //       requestAnimationFrame(render);
-  //     }
-  //     props.streamManager.addVideoElement(videoRef.current);
-  //   }
-  // }, [props.streamManager]);
 
   const [isVisible, setIsVisible] = useState(false);
   const [teachableProgress, setTeachableProgress] = useState("미션 수행하기!")
+  const [poseGuideFirst, setPoseGuideFirst] = useState("")
+  const [poseGuideSecond, setPoseGuideSecond] = useState("")
   let currentPoseTimer = 0
   let targetPoseTimer = 3 // 3초 유지
-
+  let secondPoseToggle = false
   
   useEffect(() => {
     if (!props.type) return
     console.log(props.type)
     const filePath = "/teachable_models/" + props.type + "_model/"
+    // 동작이 2가지인 미션일 경우
+    if (props.id === "5" || props.id === "10" || props.id === "11") {
+      setPoseGuideFirst(filePath + "/" + props.subType1 + "_guide.jpg")
+      setPoseGuideSecond(filePath + "/" + props.subType2 + "_guide.jpg")
+    } else { // 동작이 1개인 미션일 경우
+      setPoseGuideFirst(filePath + "/" + props.type + "_guide.jpg")
+      setPoseGuideSecond("")
+    }
     const btn = document.getElementById("teachable");
     btn.setAttribute("onclick", `init("${filePath}")`);
     btn.addEventListener("click",getCurrentPose);
-    // const btn1 = document.getElementById("off")
-    // btn1.addEventListener("click",window.deleteCanvas);
-    // btn1.setAttribute("onclick",`deleteCanvas()` )
   }, [props.type]);
   
   const getCurrentPose = () => {
@@ -51,41 +35,60 @@ const TeachableMission = (props) => {
     setTimeout(() => {
       let teachableTimer = setInterval(() => {
         let currentPose = document.getElementById("currentPose").innerHTML
-        // console.log(currentPose);
-        if (props.type !== currentPose) {
-          currentPoseTimer = 0
-          setTeachableProgress(props.type + " 동작이 감지되지 않았어요...")
-        } else if (currentPoseTimer < targetPoseTimer) {
-          currentPoseTimer += 1
-          setTeachableProgress(props.type + " 동작을 " + currentPoseTimer + "초 동안 유지 중이에요!")
-        } else {
-          setTeachableProgress(props.type + " 동작 유지 미션 완료!")
-          alert("미션완료");
-          window.deleteCanvas();
+        // 동작이 2가지인 미션일 경우
+        if (props.id === "5" || props.id === "10" || props.id === "11") {
+          if (!secondPoseToggle) {
+            if (props.subType1 !== currentPose) {
+              currentPoseTimer = 0
+              setTeachableProgress(props.subType1 + " 동작이 감지되지 않았어요...")
+            } else if (currentPoseTimer < targetPoseTimer) {
+              currentPoseTimer += 1
+              setTeachableProgress(props.subType1 + " 동작을 " + currentPoseTimer + "초 동안 유지 중이에요!")
+            } else {
+              setTeachableProgress(props.type + " 동작 유지 미션 완료!")
+              alert("미션완료");
+              window.deleteCanvas();
+    
+              props.setComplete(true);
+              setIsVisible(false);
+    
+              return clearInterval(teachableTimer)
+            }    
+          } else {
 
-          props.setComplete(true);
-          setIsVisible(false);
-
-          return clearInterval(teachableTimer)
+          }
+        } else { // 동작이 1개인 미션일 경우
+          if (props.type !== currentPose) {
+            currentPoseTimer = 0
+            setTeachableProgress(props.type + " 동작이 감지되지 않았어요...")
+          } else if (currentPoseTimer < targetPoseTimer) {
+            currentPoseTimer += 1
+            setTeachableProgress(props.type + " 동작을 " + currentPoseTimer + "초 동안 유지 중이에요!")
+          } else {
+            setTeachableProgress(props.type + " 동작 유지 미션 완료!")
+            alert("미션완료");
+            window.deleteCanvas();
+  
+            props.setComplete(true);
+            setIsVisible(false);
+  
+            return clearInterval(teachableTimer)
+          }    
         }
       }, 1000)
     }, 1000)
   }
-  // try {
-  //   // 일단 이러면 String의 형태로 오는 거 같긴 한데...
-  //   console.log(document.getElementById("currentPose").innerHTML)
-  // } catch (err) {
-  //   console.log("아직 태그 생성이 안 됐어!")
-  // }
 
+  // 1. 이미지 가이드는 넣었는가? OK
+  // 2. 동작에 대한 설명은 들어갔는가? OK
+  // 3. 2개 이상의 동작에 대한 로직이 완성되었는가?
   return (
     <>
-      {/* <div>
-        <video id="teachable" autoPlay={true} muted={props.mutedSound} ref={videoRef} />
-      </div> */}
-      {/* <Button id="off">
-        삭제
-      </Button> */}
+      <h1> {props.content} </h1>
+      <div>
+        <img src={poseGuideFirst} style={{width:"300px", height:"200px", display:poseGuideFirst?"inline":"none"}} alt="가이드1"/>
+        <img src={poseGuideSecond} style={{width:"300px", height:"200px", display:poseGuideSecond?"inline":"none"}} alt="가이드2"/>
+      </div>
       <Button id="teachable">
         미션 수행하기
       </Button>
@@ -94,9 +97,6 @@ const TeachableMission = (props) => {
         
       </div>
       <div id="label-container"></div>
-      {/* <Button onClick={getCurrentPose}>
-        미션수행
-      </Button> */}
       <div style={{display:isVisible?"block":"none"}}>{teachableProgress}</div>
     </>
   );
