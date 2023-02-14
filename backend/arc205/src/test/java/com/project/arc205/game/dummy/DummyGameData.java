378e498d24e13762311cefa5e6009907de9be269
오민성 <qww1552@naver.com>
2023-02-14 12:10:16 +0900

    FIX: 테스트 코드 수정

--- backend/arc205/src/test/java/com/project/arc205/game/dummy/DummyGameData.java
+++ backend/arc205/src/test/java/com/project/arc205/game/dummy/DummyGameData.java
@@ -4,8 +4,8 @@ import com.project.arc205.common.model.Location;
 import com.project.arc205.game.gamecharacter.model.entity.GameCharacter;
 import com.project.arc205.game.gamedata.model.entity.GameData;
 import com.project.arc205.game.gamedata.model.entity.GameSetting;
-import com.project.arc205.game.gamedata.strategy.BasicGameCharacterAssignStrategy;
 import com.project.arc205.game.room.model.entity.Room;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -18,8 +18,7 @@ public class DummyGameData {
     }
 
     public static GameData getEmptyTestGameDataFromRoom(Room testRoom) {
-        return GameData.of(testRoom.getId(), new GameSetting(),
-                new BasicGameCharacterAssignStrategy().getCharactersFromPlayers(
-                        testRoom.getPlayers()));
+        return GameData.of(testRoom.getId(), new GameSetting(), new HashMap<>(),
+                new Location(0.0, 0.0));
     }
 }
