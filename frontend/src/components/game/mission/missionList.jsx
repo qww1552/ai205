c893f23ae4ff12646af040d319ffeef945d572f9
user name <user@inflearn.com>
2023-02-14 11:17:12 +0900

    FEAT:임시

--- frontend/src/components/game/mission/missionList.jsx
+++ frontend/src/components/game/mission/missionList.jsx
@@ -7,13 +7,15 @@ const mission2 = `미션 2`
 
 const MissionList = () => {
   const job = useSelector(selectMe).player.role
+  const missions = useSelector(selectMe).player.missions
   return (
     <div>
       <Collapse style={{background:"silver"}}>
         {/* Todo: 지금은 직업이 undefined라뜸 초기화되면 지울것 */}
         <Panel header={`${job}:미션 목록`} key="1" >
-          <p>{mission1}</p>
-          <p>{mission2}</p>        
+          {missions.map((mission)=>{
+            return(<p className={mission.isComplete === true?'isComplted':'isUncompleted'}>{mission.title}</p>)
+          })}    
         </Panel>
       </Collapse>
     </div>
